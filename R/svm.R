723e2e942cdb5d0810602833f1b01d01e908c732
Nathan Skene <33265555+NathanSkene@users.noreply.github.com>
2019-08-30 22:04:29 +0100

    Removed quit() commands
    It's extremely unlikely that anyone would ever actually want to call quit() just because there is an error in the code. I've replaced them with error calls.

--- R/svm.R
+++ R/svm.R
@@ -1,24 +1,22 @@
-if(Sys.info()['sysname'] == 'Windows'){
-	if(!file.exists("../build/bin/Debug/thundersvm.dll")){
-		print("Please build the library first!")
-		quit()
-	}
-	dyn.load("../build/bin/Debug/thundersvm.dll")
-} else if(Sys.info()['sysname'] == 'Linux'){
-	if(!file.exists("../build/lib/libthundersvm.so")){
-		print("Please build the library first!")
-		quit()
-	}
-	dyn.load("../build/lib/libthundersvm.so")
-} else if(Sys.info()['sysname'] == 'Darwin'){
-    	if(!file.exists("../build/lib/libthundersvm.dylib")){
-    		print("Please build the library first!")
-    		quit()
-    	}
-    	dyn.load("../build/lib/libthundersvm.dylib")
-} else{
-    print("OS not supported!")
-    quit()
+check_location <- function(){
+  if(Sys.info()['sysname'] == 'Windows'){
+    if(!file.exists("../build/bin/Debug/thundersvm.dll")){
+      stop("Please build the library first!")
+    }
+    dyn.load("../build/bin/Debug/thundersvm.dll")
+  } else if(Sys.info()['sysname'] == 'Linux'){
+    if(!file.exists("../build/lib/libthundersvm.so")){
+      stop("Please build the library first!")
+    }
+    dyn.load("../build/lib/libthundersvm.so")
+  } else if(Sys.info()['sysname'] == 'Darwin'){
+    if(!file.exists("../build/lib/libthundersvm.dylib")){
+      stop("Please build the library first!")
+    }
+    dyn.load("../build/lib/libthundersvm.dylib")
+  } else{
+    stop("OS not supported!")
+  }
 }
 svm_train_R <-
 function(
